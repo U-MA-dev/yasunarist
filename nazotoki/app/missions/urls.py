2a2d92bfdbf82e4376ebaa1ff26b8dd82ef842ec
yuma_kawabata <kawabata@iotbase.co.jp>
2020-06-21 22:59:08 +0900

    create base index

--- nazotoki/app/missions/urls.py
+++ nazotoki/app/missions/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'missions'
 urlpatterns = [
+    path('', views.index, name="index"),
 ]
---
+++ nazotoki/app/nazotoki/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+index = IndexView.as_view()
