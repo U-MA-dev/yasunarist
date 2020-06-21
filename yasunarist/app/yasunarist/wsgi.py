5f327f2f214fe4d2ce36004276d1beda785eab9b
yuma_kawabata <kawabata@iotbase.co.jp>
2020-06-21 23:18:04 +0900

    change project name

--- yasunarist/app/yasunarist/wsgi.py
+++ yasunarist/app/yasunarist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nazotoki project.
+WSGI config for yasunarist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nazotoki.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yasunarist.settings')
 
 application = get_wsgi_application()
